68155adc5e6fa469d9182fcf683595f11ae23842
nigarlyva12 <nigarlyva12@gmail.com>
2026-09-20 16:09:32 +0200

    new changes

--- src/OOP/StudentGradeManager/Student.java
+++ src/OOP/StudentGradeManager/Student.java
@@ -19,6 +19,30 @@ public class Student {
 	public void addGrade(Grade grade) {
 		grades.add(grade);
 	}
+	
+	public void addSubject(Subject subject) {
+		attendingSubjects.add(subject);
+	}
+	
+	public double calculateAverage() {
+		int sum = 0;
+		int size = grades.size();
+		
+		if (grades.isEmpty()) return 0.0;
+		for(int i=0; i<size; i++) {
+			sum += grades.get(i).getValue();
+		}
+		return (double) sum/size;
+	}
+	
+	public Grade getGradeForSubject(Subject subject) {
+		for(Grade grade : grades) {
+			if(grade.getSubjectName().getSubjectId().equalsIgnoreCase(subject.getSubjectId()))
+				return grade;
+		}
+		return null;
+	}
+	
 	public String getStudentId() {
 		return studentId;
 	}
